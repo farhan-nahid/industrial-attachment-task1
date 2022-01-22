e6d7840bab727c81cb71efe5c650cf476930852a
gesora96 <nahidahmed9933@gmail.com>
2022-01-22 12:48:16 +0600

    done all

--- exercise1.js
+++ exercise1.js
@@ -14,7 +14,7 @@ Implement This using Javascript with any value of a , b
 */
 
 const problem1 = (a, b) => Math.pow(a, 2) + 2 * a * b + Math.pow(b, 2);
-const problem2 = (a, b) => Math.pow(a, 3) + 3 * a * 2 * b + 3 * a * b * 2 + Math.pow(b, 3);
+const problem2 = (a, b) => Math.pow(a, 3) + ( 3 * Math.pow(a, 2) * b ) + (3 * a * Math.pow(b, 2)) + Math.pow(b, 3);
 
 console.log(`problem1 result is : ${problem1(8, 9)}`);
 console.log(`problem2 result is : ${problem2(8, 9)}`);
