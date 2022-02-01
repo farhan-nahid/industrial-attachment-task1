10342b5472791405dd1ff7fabe6dce6a19d05326
gesora96 <nahidahmed9933@gmail.com>
2022-02-01 20:45:21 +0600

    task 5 added

---
+++ task-5/output/task.js
@@ -0,0 +1,12 @@
+const problem = [
+  { k1: 'v1', k2: 'b3' },
+  2,
+  { every: 'every thing' },
+  20,
+  [10, 15, [6, { prop: 'okay' }, 'Money'], ['nothing']],
+  'is',
+  ['give'],
+  [{ money: 'tk' }],
+];
+
+console.log(`Vai ${problem[4][2][2]} ${problem[5]} ${problem[2].every}. ${problem[6][0]} me ${problem[3]} ${problem[7][0].money} Now`);
